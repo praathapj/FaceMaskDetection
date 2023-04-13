e02505756bb0c22c5854e2fd2da448405f2569ad
Prathap J <praathapj@gmail.com>
2023-04-13 21:41:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ if frame is not None:
         st.error("No Mask")
 
 st.write(" ")
-st.subheader("Click for more information")
+st.write("Click for more information")
 # Source: https://stackoverflow.com/questions/74003574/how-to-create-a-button-with-hyperlink-in-streamlit
 url = 'https://github.com/praathapj/FaceMaskDetection'
 
